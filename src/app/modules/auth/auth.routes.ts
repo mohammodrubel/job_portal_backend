de1b8ee65266f492d9c7fae0ba68e376ed2c60a1
mohammodrubel <md.rubel007@hotmail.com>
2025-12-26 19:08:06 +0600

    update

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import AuthValidation from './auth.validation';
 import AuthController from './auth.controller';
-import auth from '../../middlewares/auth';
-import { UserRole } from '@prisma/client';
 
 const router = express.Router();
 
