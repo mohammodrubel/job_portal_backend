5fe667723af454b4d663eb04f37160c0a7225594
mohammodrubel <md.rubel007@hotmail.com>
2026-01-06 11:47:41 +0600

    update user skill

--- src/app/modules/userSkill/userSkill.service.ts
+++ src/app/modules/userSkill/userSkill.service.ts
@@ -2,22 +2,21 @@ import { UserSkill } from '@prisma/client';
 import prisma from '../../utils/prisma';
 
 const createSkill = async (payload: Partial<UserSkill>, userId: string) => {
-  const result = await prisma.userSkill.update({
-    where: {
-      userId,
-    },
-    data: payload,
-  });
-
-  return result;
+    const result = await prisma.userSkill.create(
+      {
+        data:{
+          name:payload.name as string,
+          category:payload.category,
+          experience:payload.experience,
+          proficiency:payload.proficiency,
+          userId:userId
+        }
+      }
+    )
+    return result
 };
 const getSingleSkill = async (id: string) => {
-  const result = await prisma.userSkill.findUnique({
-    where: {
-      userId: id,
-    },
-  });
-  return result;
+  
 };
 export const skillService  = {
   createSkill,
