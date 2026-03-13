31ec1e642c62d5ea97a06eec847ad4268331347e
mohammodrubel <md.rubel007@hotmail.com>
2026-03-14 03:38:05 +0600

    job application create successfully complete

--- src/app/modules/JobApplication/JobApplication.service.ts
+++ src/app/modules/JobApplication/JobApplication.service.ts
@@ -1,18 +1,40 @@
 import { JobApplication } from "@prisma/client"
 import prisma from "../../utils/prisma"
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
 
-const applyJob = async (userId:string,payload:JobApplication) => {
-    const result = await prisma.jobApplication.create(
-        {
-            data:{
-                ...payload,
-                userId:userId
-            } 
-        }
-    )
-    return result
-}
-const getAllApply = async () => { 
+const applyJob = async (userId: string, payload: JobApplication) => {
+  try {
+    const result = await prisma.$transaction(async (tx) => {
+      const application = await tx.jobApplication.create({
+        data: {
+          jobId: payload.jobId,
+          userId,
+          resumeId: payload.resumeId,
+          coverLetter: payload.coverLetter,
+        },
+      });
+
+      await tx.job.update({
+        where: {
+          id: payload.jobId,
+        },
+        data: {
+          applicationsCount: {
+            increment: 1,
+          },
+        },
+      });
+
+      return application;
+    });
+
+    return result;
+  } catch (error) {
+    throw new AppError(httpStatus.CONFLICT, "You already applied for this job");
+  }
+};
+const getAllApply = async () => {
 
 }
 const getSingleJob = async () => { }
